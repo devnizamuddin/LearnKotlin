7864134cdf851cf77a95e7ab16c9fec2144f3fa7
nizamuddinshamrat <nshamrat7@gmail.com>
2023-06-30 02:27:39 +0600

    Comment Style updated on CostCalculation.kt

--- main/keyword/object/CostCalculation.kt
+++ main/keyword/object/CostCalculation.kt
@@ -1,8 +1,11 @@
 package main.keyword.`object`
 
 object CostCalculation {
-    // You can use any variable or function without creating object by using object keyword
-    // Mostly used to store const value or to use util function
+
+    /** You can use any variable or function without creating object by using object keyword
+     * Mostly used to store const value or to use util function
+     */
+
     var roomRent = 50
     var pocketCost = 90
 
