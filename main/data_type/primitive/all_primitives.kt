67a21762b228060d950100785e0544d5299bc28d
nizamuddinshamrat <nshamrat7@gmail.com>
2023-06-30 16:59:08 +0600

    Learn "Byte" primitive data type

--- main/data_type/primitive/all_primitives.kt
+++ main/data_type/primitive/all_primitives.kt
@@ -1,6 +1,15 @@
 package main.data_type.primitive
 
-
 fun main() {
+
+    /**=============================================== All Primitives ===============================================**/
+
+
+    /**-------------------------Byte-------------------------*/
+
+    /* 16 bits  | -32,768 to 32,767 | Signed integer */
+
+    val byteValue: Byte = 127
+    println(byteValue)
 
 }
